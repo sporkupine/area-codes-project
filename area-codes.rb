5242f165673958b70f9386444c8116e95fa2a2be
sporkupine <coffeechief25@gmail.com>
2022-02-24 14:39:20 -0600

    initial commit, set up exercise

---
+++ area-codes.rb
@@ -0,0 +1,23 @@
+area_codes = {
+  "newyork" => "212",
+  "newbrunswick" => "732",
+  "edison" => "908",
+  "plainsboro" => "609",
+  "sanfrancisco" => "301",
+  "miami" => "305",
+  "paloalto" => "650",
+  "evanston" => "847",
+  "orlando" => "407",
+  "lancaster" => "717",
+  "paducah" => "270"
+}
+
+# Get city names from the hash
+def get_city_names(hash)
+
+end
+
+# Get area code based on given hash and key
+def get_area_code(hash, key)
+  
+end
